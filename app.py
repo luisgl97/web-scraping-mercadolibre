2156e33db21857e27188cfa843dc0a556c5878f6
luisgl97 <luisaltlv2010@gmail.com>
2022-02-26 18:35:21 -0500

    actualizacion

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, render_template, request, Response
 import json
-from functions import todosProductos, limiteProductos
-#ga
-app = Flask(__name__)
 
+from numpy import product
+from functions import todosProductos, limiteProductos
+import requests
+
+app = Flask(__name__)
+# Api url: https://web-scraping-mercado-libre.herokuapp.com/
+#url en postman : https://web-scraping-mercado-libre.herokuapp.com/mercadoLibre 
 @app.route('/mercadoLibre',methods=["GET"])
 def mercadoLibre():
     url='https://listado.mercadolibre.com.pe/'
@@ -22,6 +26,43 @@ def mercadoLibre():
             "precios":precios
         }
     })
+
+@app.route("/descargarInfo",methods=["GET","POST"])
+def descargarInfo():
+   
+    if request.method == "POST":
+        
+        producto =  request.form["producto"]
+        limite = request.form["limite"]
+        
+        print(limite)
+        
+        #Consumir API
+        r = requests.get('http://localhost:5000/mercadoLibre', json={"producto":producto, "limite":int(limite)})
+        print(r.status_code)
+        print(producto,limite)
+        
+        if r.status_code==200:
+            data = json.loads(r.text)
+            # print(data["datos"]["precios"])
+            # print(data["datos"]["titulos"])
+            # print(data["datos"]["urls"])
+            t = ""
+            for i,j,z in zip(data["datos"]["precios"],data["datos"]["titulos"],data["datos"]["urls"]):
+                print(i,j,z)
+                t+=f"{j}|{z}|{i}\n"
+                
+            return Response(
+                t,
+                mimetype="text",
+                headers = {
+                    "Content-disposition":"attachment;filename=datos.txt"
+                }
+            )
+            #print(data)
+        return "error"
+        pass
+    return render_template('index.html')
 
 if __name__=="__main__":
     app.run(host="0.0.0.0", debug=True)
