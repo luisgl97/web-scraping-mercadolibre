a996f67efb36bfc2fc88cc6dae4f872a38e5a1f6
Luis Gonzales <luis.gonzales35@unmsm.edu.pe>
2022-03-02 22:12:01 -0500

    Consumir API

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def descargarInfo():
         print(limite)
         
         #Consumir API
-        r = requests.get('https://web-scraping-mercado-libre.herokuapp.com/mercadoLibre', json={"producto":producto, "limite":int(limite)})
+        r = requests.get('https://web-scraping-mercado-libre.herokuapp.com/', json={"producto":producto, "limite":int(limite)})
         print(r.status_code)
         print(producto,limite)
         
